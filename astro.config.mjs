fb035c9695f742025c1931943d6b1469d1be9ed7
Freilyn Bernabe <freilyn@bernabe.dev>
2025-02-27 17:38:48 -0400

    feat: Add Vercel build and environment variable configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
+import { loadEnv } from "vite";
+const { WEB_URL } = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
+  site: WEB_URL,
 
   vite: {
     plugins: [tailwindcss()]
