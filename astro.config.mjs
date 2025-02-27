e2b40f778019eac72317c60f69e019e0454a8091
Freilyn Bernabe <freilyn@bernabe.dev>
2025-02-27 13:53:40 -0400

    feat: Add sitemap and server port configuration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
 
+import sitemap from '@astrojs/sitemap';
+import { loadEnv } from "vite";
+const { PORT } = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -15,5 +19,11 @@ export default defineConfig({
 
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  integrations: [sitemap()],
+
+  server: {
+    port: PORT ? +PORT : 4321
+  }
 });
