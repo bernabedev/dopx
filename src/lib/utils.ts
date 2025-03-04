f2022a47734b9c3537227532ddd69b4850e89168
Freilyn Bernabe <freilyn@bernabe.dev>
2025-03-04 09:30:18 -0400

    feat: Update layout, add date, utils functions, and currency conversion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,17 @@
 export function formatDate(dateString: string) {
-  const date = new Date(dateString);
-  return date.toLocaleDateString("es-DO", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
+  const date = new Date(dateString)
+  return date.toLocaleDateString('es-DO', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  })
 }
 
-export function formatNumber(num: string | number) {
-  return Number(num).toFixed(2);
+export function formatNumber(num: string | number, locale = 'DOP') {
+  return new Intl.NumberFormat('es-DO', {
+    style: 'decimal',
+    currency: locale,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(Number(num))
 }
